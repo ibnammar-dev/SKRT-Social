74b382b3ba8e5c61d3ad2d235375774b5c3c1778
ibnammar-dev <ibnammar.dev@gmail.com>
2025-01-25 23:47:17 +0100

    my own css changes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProductController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 } 
